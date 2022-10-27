3dba74fcb04f841f5cdaeb1903c0074f7e203a3d
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-10-27 14:07:21 +0200

    h

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button");
+const counter = document.querySelector(".counter");
+const inputs = document.querySelector(".inputs");
+
+const number = 0;
+button.onmousedown(function() {
+number++;
+
+
+})
