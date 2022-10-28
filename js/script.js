9b276b18a8dedd8373c349f883caa4811d64ad05
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-10-28 14:17:10 +0200

    done

--- js/script.js
+++ js/script.js
@@ -3,15 +3,43 @@ const counter = document.querySelector(".counter");
 const inputs = document.querySelector(".inputs");
 
 
+let intervalId;
+let number;
 
-let number = 0;
 function hold() {
-number++;
-if (button.onclick) {
-    console.log(number);
-}
+    number = 0;
+button.textContent = "Release to stop counting!";
+
+
+intervalId = setInterval (function() {
+    
+    number++;
+    counter.innerHTML = number;
+    inputs.innerHTML = "";
+    
+}, 1000);
 
 }
 
-setInterval(hold, 500);
+function release() {
+    clearInterval(intervalId);
+    button.textContent = "Press and hold to count";
+    for (let i = 1; i <= number; i++) {
 
+        inputs.innerHTML += `<input name="input${i}" id="input${i}" placeholder="Input ${i}" />`;   
+    }
+
+}
+
+
+
+
+
+////////
+
+
+button.addEventListener("mousedown", hold);
+button.addEventListener("mouseup", release);
+// button.onmousedown = hold;
+// button.onmouseup = release;
+// let intervalId = setInterval (hold, 1000);
